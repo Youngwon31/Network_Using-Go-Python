a9edf2e858d77a6b838aedf4eeed7df7af9826c3
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2024-02-23 16:22:58 -0500

    GO does not use syslog on Windows. So we imported logrus and applied it. logrus supports various log levels like syslog. Wrote some initial code.

--- server.go
+++ server.go
@@ -4,31 +4,41 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
+// Global logger instance
+var log = logrus.New()
+
 func main() {
+
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&logrus.JSONFormatter{})
+
 	// Set up a log file and include log...
 	// study:
 	// The os.OpenFile function is the standard Go function for opening files.
 	// os.O_CREATE: This flag creates the file if it doesn't exist.
 	// os.O_WRONLY:  This flag is for opening files in write-only mode
 
-	logFile, err := os.OpenFile("Yseo8834_jiukim9115_server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatal("Error opening log file:", err) // log.Fatal writes a log message and then calls os.Exit(1) to exit the program
+	// Note: https://github.com/sirupsen/logrus
+	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(logFile)
+	} else {
+		log.Info("Failed to log to file, using default stderr")
 	}
-	defer logFile.Close() // The defer keyword schedules a specific function to run immediately before the current function (main or another function) returns.
 
 	// Intended to set log output to logFile.
 	log.SetOutput(logFile)
 
-	fmt.Println("Hello, World!")
-
 	// UDP > net.Listenpacket, TCP > net.listen
 	listener, err := net.Listen("tcp", ":8080") // If an error occurs in this action, it is stored in the err variable.
+
+	fmt.Println("Hello, World!")
 
 	// nil means 'nothing' or 'no error' in Go
 	if err != nil {
@@ -56,6 +66,10 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	log.Printf("Connected from client %v\n", conn.RemoteAddr())
-	// 여기에 메시지 읽기 및 로깅 코드를 추가할 것!!!
+	// Using Logrus to log the message
+	log.WithFields(logrus.Fields{
+		"client": conn.RemoteAddr(),
+	}).Info("Client connected")
+
+	// 메시지 읽기 및 로깅 로직을 여기에 구현...
 }
